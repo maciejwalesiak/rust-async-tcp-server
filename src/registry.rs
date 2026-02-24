58db7b0edef809d95dfd5b0fcf7efa7404858326
Maciej Walesiak <mwalesiak@mwiz.pl>
2026-02-24 18:05:17 +0100

    build fix

--- src/registry.rs
+++ src/registry.rs
@@ -23,7 +23,7 @@ use tokio::sync::broadcast;
 pub const MSG_BROADCAST_ID: u16 = 0xFFFF;
 pub const MSG_REGISTRY_ID: u16 = 0;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Message {
     src_id: u16,
     dst_id: u16,
